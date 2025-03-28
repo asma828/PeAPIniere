b4dd217b615048871e8c912cff93a1669c08f26f
asma828 <asmalachhab66@gmail.com>
2025-03-28 12:16:29 +0000

    add authentification routes

--- PeAPIniere/routes/api.php
+++ PeAPIniere/routes/api.php
@@ -1,8 +1,12 @@
 <?php
 
+use App\Http\Controllers\OrderController;
+use App\Http\Controllers\StatsController;
 use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
-
+use App\Http\Controllers\AuthController;
+use App\Http\Controllers\CategoryController;
+use App\Http\Controllers\PlantController;
 /*
 |--------------------------------------------------------------------------
 | API Routes
@@ -17,3 +21,16 @@ use Illuminate\Support\Facades\Route;
 Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
     return $request->user();
 });
+
+
+Route::post('/register', [AuthController::class, 'register']);
+Route::post('/login', [AuthController::class, 'login']);
+
+Route::middleware('auth:api')->group(function () {
+    Route::post('/logout', [AuthController::class, 'logout']);
+});
+
+
+
+
+
