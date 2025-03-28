08f558ff67b941df79f27f574f664a924696e80b
asma828 <asmalachhab66@gmail.com>
2025-03-28 11:18:05 +0000

    add role middleware to kernel

--- PeAPIniere/app/Http/Kernel.php
+++ PeAPIniere/app/Http/Kernel.php
@@ -65,4 +65,12 @@ class Kernel extends HttpKernel
         'throttle' => \Illuminate\Routing\Middleware\ThrottleRequests::class,
         'verified' => \Illuminate\Auth\Middleware\EnsureEmailIsVerified::class,
     ];
+
+    protected $routeMiddleware = [
+        'auth' => \Tymon\JWTAuth\Http\Middleware\Authenticate::class,
+        'role' => \App\Http\Middleware\RoleMiddleware::class,
+
+    ];
+    
 }
+
