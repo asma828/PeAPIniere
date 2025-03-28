0d19cba3b796260f8be9ce5a6670ccd22c8f17a8
asma828 <asmalachhab66@gmail.com>
2025-03-28 11:30:38 +0000

    add service providers to link interfaces with their implementation

--- PeAPIniere/app/Providers/AppServiceProvider.php
+++ PeAPIniere/app/Providers/AppServiceProvider.php
@@ -2,6 +2,12 @@
 
 namespace App\Providers;
 
+use App\Repositories\CategoryDAO;
+use App\Repositories\Interfaces\CategoryDAOInterface;
+use App\Repositories\Interfaces\OrderRepositoryInterface;
+use App\Repositories\Interfaces\PlantDAOInterface;
+use App\Repositories\OrderRepository;
+use App\Repositories\PlantDAO;
 use Illuminate\Support\ServiceProvider;
 
 class AppServiceProvider extends ServiceProvider
@@ -11,7 +17,9 @@ class AppServiceProvider extends ServiceProvider
      */
     public function register(): void
     {
-        //
+        $this->app->bind(PlantDAOInterface::class, PlantDAO::class);
+        $this->app->bind(CategoryDAOInterface::class, CategoryDAO::class);
+        $this->app->bind(OrderRepositoryInterface::class, OrderRepository::class);
     }
 
     /**
